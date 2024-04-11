b18afc9d0f9c2dcda4dd91a2e7bd74c2598b85cb
Bianor Araujo <bianor.araujo@gmail.com>
2024-04-11 01:32:26 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: "/projeto1",
+  base: "/projeto1/dist",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
